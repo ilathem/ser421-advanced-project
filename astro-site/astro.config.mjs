f295e967574ee8ec104300a66f498e74414f7450
ilathem <isaiahlathem@gmail.com>
2022-09-20 14:28:20 -0500

    Added vue and svelte dependencies

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import react from '@astrojs/react'
+import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }), 
-    mdx(),
-    react(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), react(), vue(), svelte()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
