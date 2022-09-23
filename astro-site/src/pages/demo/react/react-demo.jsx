035154dcaaed291fc00a8dcb006ecec190111f34
ilathem <isaiahlathem@gmail.com>
2022-09-23 08:41:34 -0500

    finish react demo

--- astro-site/src/pages/demo/react/react-demo.jsx
+++ astro-site/src/pages/demo/react/react-demo.jsx
@@ -3,21 +3,14 @@ import './todo.css';
 
 export default function ReactDemo() {
   // user state
-  const [user, setUser] = useState('f');
+  const [user, setUser] = useState('');
   // array of todo items
   const [todos, setTodos] = useState([]);
-  // state for storing current edited todo item
-  const [currentTodoTitle, setCurrentTodoTitle] = useState('');
-  const [currentTodoDate, setCurrentTodoDate] = useState(new Date());
-  const [currentTodoPercent, setCurrentTodoPercent] = useState('');
 
-  // when current todo item changes, update array
+  // when array changes, update local storage
   useEffect(() => {
-    console.log('current todo has changed!');
-    
-  }, [currentTodoDate, currentTodoTitle, currentTodoPercent]);
-
-  function newTask() {}
+    window.localStorage.setItem(user, JSON.stringify(todos))
+  }, [todos])
 
   function logIn() {
     let username = prompt('Please enter your name');
@@ -26,10 +19,98 @@ export default function ReactDemo() {
       setTodos(JSON.parse(window.localStorage.getItem(username)));
     }
     // new user
-    setUser(username)
+    setUser(username);
+    // console.log(todos)
   }
 
-  function editTask(todoTitle) {}
+  function addNewTodo(todo) {
+    if (!todos) setTodos(todo) // if first one
+    else {
+      let todoArray = [...todos, todo]
+      // https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
+      let sortedTodos = todoArray.sort((a,b)=>new Date(a.date).getTime()-new Date(b.date).getTime());
+      // console.log({todoArray, sortedTodos})
+      setTodos(sortedTodos)
+    }
+  }
+
+  function newTask() {
+    let title = prompt("Enter the task name")
+    let titlesArray = todos.map(todo => todo.title)
+    while (titlesArray.includes(title)) {
+      title = prompt("That task already exists! Enter another name")
+    }
+    let date = prompt("Enter future due date in format <MM/DD/YYYY>")
+    while (!Date.parse(date) || Date.parse(date) < Date.now()) {
+      date = prompt("Invalid syntax, use format <MM/DD/YYYY> in the future")
+    }
+    let percent = prompt("Enter completion percentage, value from 0 to 100");
+    while (percent < 0 || percent > 100) {
+      percent = prompt("Enter a value from 0 to 100")
+    }
+    addNewTodo({title, date, percent})
+  }
+
+  function editTask(todoTitle) {
+    let newTitle = prompt("What is the new title?")
+    let titlesArray = todos.map(todo => todo.title)
+    while (titlesArray.includes(newTitle)) {
+      newTitle = prompt("That name already exists! Enter another name")
+    }
+    let updatedArray = todos.map(todo => {
+      if (todo.title === todoTitle) {
+        return ({
+          title: newTitle,
+          date: todo.date,
+          percent: todo.percent
+        })
+      } else {
+        return todo; 
+      }
+    })
+    setTodos(updatedArray)
+    // console.log('running edit task')
+  }
+
+  function editDate(todoTitle) {
+    // console.log("running edit date")
+    let date = prompt("Enter future due date in format <MM/DD/YYYY>")
+    while (!Date.parse(date) || Date.parse(date) < Date.now()) {
+      date = prompt("Invalid syntax, use format <MM/DD/YYYY> in the future")
+    }
+    let updatedArray = todos.map(todo => {
+      if (todo.title === todoTitle) {
+        return ({
+          title: todo.title,
+          date: date,
+          percent: todo.percent
+        })
+      } else {
+        return todo;
+      }
+    })
+    let sortedTodos = updatedArray.sort((a,b)=>new Date(a.date).getTime()-new Date(b.date).getTime());
+    setTodos(sortedTodos)
+  }
+
+  function editPercent(todoTitle) {
+    let percent = prompt("Enter completion percentage, value from 0 to 100");
+    while (percent < 0 || percent > 100) {
+      percent = prompt("Enter a value from 0 to 100")
+    }
+    let updatedArray = todos.map(todo => {
+      if (todo.title === todoTitle) {
+        return ({
+          title: todo.title,
+          date: todo.date,
+          percent: percent
+        })
+      } else {
+        return todo; 
+      }
+    })
+    setTodos(updatedArray)
+  }
 
   return (
     <>
@@ -46,41 +127,46 @@ export default function ReactDemo() {
       <br />
       {user && (
         <table style={{ width: '100%' }}>
-          <tr className='toprow'>
-            <td style={{ height: '50px', width: '33.33%' }}>
-              <b>Task</b>
-            </td>
-            <td style={{ height: '50px', width: '33.33%' }}>
-              <b>Due Date</b>
-            </td>
-            <td style={{ height: '50px', width: '33.33%' }}>
-              <b>Progress</b>
-            </td>
-          </tr>
-          {todos.map((todo) => {
-            return (
-              <tr className='itemrow' key={todo}>
-                <td
-                  style={{ height: '50px', width: '33.33%' }}
-                  onClick={editTask(todo)}
-                >
-                  {todo.title}
-                </td>
-                <td
-                  style={{ height: '50px', width: '33.33%' }}
-                  onClick={editDate(todo)}
-                >
-                  {todo.date}
-                </td>
-                <td
-                  style={{ height: '50px', width: '33.33%' }}
-                  onClick={editPercent(todo)}
-                >
-                  {todo.percent}%
-                </td>
-              </tr>
-            );
-          })}
+          <thead>
+            <tr className='toprow'>
+              <td style={{ height: '50px', width: '33.33%' }}>
+                <b>Task</b>
+              </td>
+              <td style={{ height: '50px', width: '33.33%' }}>
+                <b>Due Date</b>
+              </td>
+              <td style={{ height: '50px', width: '33.33%' }}>
+                <b>Progress</b>
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => {
+              return (
+                <tr className='itemrow' key={todo.title}>
+                  <td
+                    style={{ height: '50px', width: '33.33%' }}
+                    onClick={() => editTask(todo.title)}
+                  >
+                    {todo.title}
+                  </td>
+                  <td
+                    style={{ height: '50px', width: '33.33%' }}
+                    onClick={() => editDate(todo.title)}
+                  >
+                    {todo.date}
+                  </td>
+                  <td
+                    style={{ height: '50px', width: '33.33%' }}
+                    className={`progress-${todo.percent < 33 ? "1" : todo.percent < 67 ? "2" : "3"}`}
+                    onClick={() => editPercent(todo.title)}
+                  >
+                    {todo.percent}%
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       )}
     </>
